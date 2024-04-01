083f6da159d84de3afa72bbed3023a17c2979ef0
sunpanyu <996251389@qq.com>
2024-04-01 16:43:22 +0800

    contextMenus

--- Extension_API/chrome.contextMenus/background.js
+++ Extension_API/chrome.contextMenus/background.js
@@ -1,16 +1,49 @@
 chrome.runtime.onInstalled.addListener(function () {
+  // 定义菜单
+  // const menuList = [
+  //   {
+  //     id: "1",
+  //     title: "水果类",
+  //     children: [
+  //       { id: "1-1", title: "苹果", parentId: "1" },
+  //       { id: "1-2", title: "梨子", parentId: "1" },
+  //       { id: "1-3", title: "西瓜", parentId: "1" },
+  //     ],
+  //   },
+  // ];
+
+  // 创建菜单
+  // function createMenu(menu) {
+  //   chrome.contextMenus.create(
+  //     {
+  //       id: menu.id,
+  //       title: menu.title,
+  //       // contexts: ["selection"],
+  //       parentId: menu.parentId,
+  //       documentUrlPatterns: ["https://*.baidu.com/*"],
+  //     },
+  //     function () {
+  //       if (Array.isArray(menu.children)) {
+  //         menu.children.forEach(createMenu);
+  //       }
+  //     }
+  //   );
+  // }
+
+  // menuList.forEach(createMenu);
+
+
+
   chrome.contextMenus.create({
-    id: "my-menu-item",
-    title: "My Context Menu Item",
-    contexts: ["selection"],
-    onClick: function(info, tab) {
-      console.log("Menu item clicked", info, tab);
-    }
-  }, function() {
-    if (chrome.runtime.lastError) {
-      console.error("Error adding context menu item:", chrome.runtime.lastError.message);
-    } else {
-      console.log("Context menu item added successfully!");
-    }
+    id: "1",
+    title: "更新菜单",
   });
+
+  // 更新菜单
+  function updateMenu(id, title) {
+    chrome.contextMenus.update(id, {
+      title,
+    });
+  }
+  updateMenu("1", "更新菜单2");
 });
